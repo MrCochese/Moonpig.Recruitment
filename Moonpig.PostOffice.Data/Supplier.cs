9bdd7a086726290f20cfc5c7136928e5cd0ab388
Rick <rick.bolton@gmail.com>
2019-09-19 22:38:17 +0100

    Create calculate receive date method on Supplier

--- Moonpig.PostOffice.Data/Supplier.cs
+++ Moonpig.PostOffice.Data/Supplier.cs
@@ -1,13 +1,18 @@
 ﻿namespace Moonpig.PostOffice.Data
 {
+    using System;
+
     public class Supplier
     {
         public int SupplierId { get; set; }
 
-
         public string Name { get; set; }
 
-
         public int LeadTime { get; set; }
+
+        public DateTime CalculateReceiveDate(DateTime orderDate)
+        {
+            return orderDate.AddDays(LeadTime);
+        }
     }
 }
---
+++ Moonpig.PostOffice.Tests/SupplierTests.cs
@@ -0,0 +1,24 @@
+namespace Moonpig.PostOffice.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Api.Controllers;
+    using Data;
+    using Shouldly;
+    using Xunit;
+
+    public class SupplierTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 4)]
+        [InlineData(5, 5)]
+        public void LeadTimeFromMonday(int leadTime, int expectedDays)
+        {
+            var supplier = new Supplier() { LeadTime = leadTime };
+            supplier.CalculateReceiveDate(new DateTime(2019, 9, 16)).ShouldBe(new DateTime(2019, 9, 16).AddDays(expectedDays));
+        }
+    }
+}
